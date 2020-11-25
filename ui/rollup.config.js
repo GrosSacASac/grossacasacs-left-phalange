2536b2e1d59ae2e2882a031c252230dda15f3022
Cyril Walle <cyril.walle@protonmail.com>
2020-11-25 12:22:59 +0100

    split rollup config

---
+++ ui/rollup.config.js
@@ -0,0 +1,16 @@
+import { terser } from "rollup-plugin-terser";
+
+
+export default [
+  {
+    input: `ui/source/js/main.js`,
+    output: [
+        {
+            file: `ui/source/js/main.min.js`,
+            format: `esm`,
+          },
+    ],
+    plugins: [terser()],
+  },
+];
+
